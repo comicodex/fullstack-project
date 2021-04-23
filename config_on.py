86d005140b6b21b9cbc0f2660f48ac948ac6be1d
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2021-04-23 14:55:53 +0100

    last mod

---
+++ config_on.py
@@ -0,0 +1,6 @@
+#BASE CONFIG
+MONGODB_NAME = 'mongo_test'
+MONGO_URI = 'mongodb+srv://lorenzodemiri:qC5FpXn5amv2DuH0@cluster0.phbkj.mongodb.net/mongo_test?retryWrites=true&w=majority'
+SECRET_KEY = 'testkey_v2'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = 'uploads'
